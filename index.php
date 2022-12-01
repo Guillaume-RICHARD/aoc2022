1e2d87361cad8254ee38ac4a8c2673ee555d9ca2
Guillaume RICHARD <g.jf.richard@gmail.com>
2022-12-01 21:14:49 +0100

    résolution day 01

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Day 1 : <a href='/src/Day01/index.php'>Day01</a>";
---
+++ src/Day01/index.php
@@ -0,0 +1,33 @@
+<?php
+
+// Lire le fichier Txt
+$fichier = file("../../inputs/Day1/input-day1.txt");
+$total = count($fichier);
+
+$calories = [];
+$elve = 0;
+
+for ($i=0; $i < $total; $i++) {
+    if (strlen(($fichier[$i])) !== 1) {
+        $calories[$elve][] .= $fichier[$i];
+    } else if (strlen(($fichier[$i])) === 1) {
+        $elve++;
+    }
+}
+
+$elve = 1;
+
+// faire des sommes par Elf
+foreach ($calories as $key => $elf) {
+    $tmp[$key] = array_sum($elf);
+
+    $elve++;
+}
+unset($calories);
+$calories = $tmp;
+rsort($calories, SORT_NUMERIC);
+
+$sum = $calories[0] + $calories[1] + $calories[2];
+
+// Retourner l'Elf qui aura le plus de calorie.
+echo "L'elfe ayant eu le plus de calorie : " . max($calories). "<br />Somme de calorie des 3 premiers elfes : ". $sum;
