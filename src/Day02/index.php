1805019440cd87acd39399f550084040551b3b86
Guillaume RICHARD <g.jf.richard@gmail.com>
2022-12-04 14:28:44 +0100

    Résolution Day02

--- src/Day02/index.php
+++ src/Day02/index.php
@@ -1,33 +1,51 @@
 <?php
 
 // Lire le fichier Txt
-$fichier = file("../../inputs/Day1/input-day01.txt");
-$total = count($fichier);
+$data = file("../../inputs/Day02/input-day2.txt");
+$total = count($data);
+$scorefinal_1 = 0;
 
-$calories = [];
-$elve = 0;
+$move = ["X" => 1, "Y" => 2, "Z" => 3];
+// A for Rock, B for Paper, and C for Scissors
+// X for Rock, Y for Paper, and Z for Scissors
+$fight_turn_1 = [
+    "0" => ["A", "B", "C"],
+    "X" => ["3", "0", "6"],
+    "Y" => ["6", "3", "0"],
+    "Z" => ["0", "6", "3"],
+];
 
-for ($i=0; $i < $total; $i++) {
-    if (strlen(($fichier[$i])) !== 1) {
-        $calories[$elve][] .= $fichier[$i];
-    } else if (strlen(($fichier[$i])) === 1) {
-        $elve++;
-    }
+foreach ($data as $key => $item) {
+    $item = trim($item);
+    [$adversaire, $perso] = explode(" ", $item);
+
+    $infos = array_search($adversaire, $fight_turn_1[0]);
+    $scorefinal_1 += $fight_turn_1[$perso][$infos] + $move[$perso];
 }
 
-$elve = 1;
+// ----------------------------------------------------------------
+$scorefinal_2 = 0;
+// A for Rock, B for Paper, and C for Scissors : // A > B > C > A
+// X = Lose, Y = Draw, and Z = Win
+// 1 for Rock, 2 for Paper, and 3 for Scissors
+$move = ["X" => 0, "Y" => 3, "Z" => 6];
+$choice = ["A" => 1, "B" => 2, "C" => 3];
 
-// faire des sommes par Elf
-foreach ($calories as $key => $elf) {
-    $tmp[$key] = array_sum($elf);
+$fight_turn_2 = [
+    "0" => ["A", "B", "C"],
+    "X" => ["C", "A", "B"],
+    "Y" => ["A", "B", "C"],
+    "Z" => ["B", "C", "A"],
+];
 
-    $elve++;
+foreach ($data as $key => $item) {
+    $item = trim($item);
+    [$adversaire, $resultat] = explode(" ", $item);
+
+    $infos = array_search($adversaire, $fight_turn_2[0]);
+    $perso = $fight_turn_2[$resultat][$infos];
+    $scorefinal_2 += $move[$resultat] + $choice[$perso];
 }
-unset($calories);
-$calories = $tmp;
-rsort($calories, SORT_NUMERIC);
 
-$sum = $calories[0] + $calories[1] + $calories[2];
-
-// Retourner l'Elf qui aura le plus de calorie.
-echo "L'elfe ayant eu le plus de calorie : " . max($calories). "<br />Somme de calorie des 3 premiers elfes : ". $sum;
+echo "Resultat 1 : " . $scorefinal_1;
+echo "<br />Resultat 2 : " . $scorefinal_2;
